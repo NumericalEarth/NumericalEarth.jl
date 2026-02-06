387b6b0c73c09595dd0ec067a14ec8f9cf494947
Simone Silvestri <silvestri.simone0@gmail.com>
2026-02-06 12:21:11 +0100

    try out the new changes, copying from Breeze

---
+++ docs/deploy.jl
@@ -0,0 +1,17 @@
+using Documenter
+
+@info "Cleaning up temporary .jld2 and .nc output created by doctests or literated examples..."
+
+for (root, _, filenames) in walkdir(@__DIR__)
+    for file in filenames
+        if any(occursin(file), (r"\.jld2$", r"\.nc$"))
+            rm(joinpath(root, file))
+        end
+    end
+end
+
+deploydocs(repo = "github.com/NumericalEarth/NumericalEarthDocumentation.git",
+           versions = ["stable" => "v^", "dev" => "dev", "v#.#.#" => "v#.#.#"],
+           forcepush = true,
+           devbranch = "main",
+           push_preview = all(!isempty, (get(ENV, "GITHUB_TOKEN", ""), get(ENV, "DOCUMENTER_KEY", ""))))
