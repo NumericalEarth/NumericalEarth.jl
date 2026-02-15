a8f0b3a59a11859892f51398f2f2adf24d6818a3
Simone Silvestri <silvestri.simone0@gmail.com>
2026-02-15 20:51:40 +0100

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -33,7 +33,7 @@ examples = [
     Example("One-degree ocean--sea ice simulation", "one_degree_simulation", false),
     Example("Near-global ocean simulation", "near_global_ocean_simulation", false),
     Example("Global climate simulation", "global_climate_simulation", false),
-    Example("Veros ocean simulation" => "literated/veros_ocean_forced_simulation.md", true), # For the moment just to test, then put it to false
+    Example("Veros ocean simulation", "veros_ocean_forced_simulation.md", true), # For the moment just to test, then put it to false
 ]
 
 # Developer examples from docs/src/developers/ directory
