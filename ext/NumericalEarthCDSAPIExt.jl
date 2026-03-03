13e4fab5f83da180004793af52ffc58d1b1ad600
Eliot Quon <eliot@aeolus.earth>
2026-03-03 11:16:04 -0700

    Add ERA5 pressure-levels downloader

--- ext/NumericalEarthCDSAPIExt.jl
+++ ext/NumericalEarthCDSAPIExt.jl
@@ -8,6 +8,7 @@ using Oceananigans.DistributedComputations: @root
 
 using Dates
 using NumericalEarth.DataWrangling.ERA5: ERA5Metadata, ERA5Metadatum, ERA5_dataset_variable_names
+using NumericalEarth.DataWrangling.ERA5: ERA5PressureMetadata, ERA5PressureMetadatum, ERA5PL_dataset_variable_names
 
 import NumericalEarth.DataWrangling: download_dataset
 
@@ -85,6 +86,55 @@ function download_dataset(meta::ERA5Metadatum; skip_existing=true)
     # Perform the download using CDSAPI
     @root begin
         CDSAPI.retrieve("reanalysis-era5-single-levels", request, output_path)
+    end
+
+    return output_path
+end
+
+#####
+##### ERA5 pressure-level download
+#####
+
+function download_dataset(metadata::ERA5PressureMetadata; kwargs...)
+    paths = Array{String}(undef, length(metadata))
+    for (m, metadatum) in enumerate(metadata)
+        paths[m] = download_dataset(metadatum; kwargs...)
+    end
+    return paths
+end
+
+function download_dataset(meta::ERA5PressureMetadatum; skip_existing=true)
+    output_directory = meta.dir
+    output_filename  = NumericalEarth.DataWrangling.metadata_filename(meta)
+    output_path      = joinpath(output_directory, output_filename)
+
+    skip_existing && isfile(output_path) && return output_path
+    mkpath(output_directory)
+
+    variable_name = ERA5PL_dataset_variable_names[meta.name]
+    date  = meta.dates
+    year  = string(Dates.year(date))
+    month = lpad(string(Dates.month(date)), 2, '0')
+    day   = lpad(string(Dates.day(date)),   2, '0')
+    hour  = lpad(string(Dates.hour(date)),  2, '0') * ":00"
+
+    request = Dict(
+        "product_type"    => ["reanalysis"],
+        "variable"        => [variable_name],
+        "pressure_level"  => [string(p) for p in meta.dataset.levels],
+        "year"            => [year],
+        "month"           => [month],
+        "day"             => [day],
+        "time"            => [hour],
+        "data_format"     => "netcdf",
+        "download_format" => "unarchived",
+    )
+
+    area = build_era5_area(meta.bounding_box)
+    isnothing(area) || (request["area"] = area)
+
+    @root begin
+        CDSAPI.retrieve("reanalysis-era5-pressure-levels", request, output_path)
     end
 
     return output_path
