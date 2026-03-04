f5e9464277a55c4b319c52dadf3948131dcd8d42
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-03-04 06:03:05 +0100

    diagnostics return AbstractOperations

--- src/Diagnostics/interface_fluxes.jl
+++ src/Diagnostics/interface_fluxes.jl
@@ -11,8 +11,7 @@ Return the two-dimensional frazil temperature flux (K m s⁻¹) in a coupled `es
 function frazil_temperature_flux(esm::EarthSystemModel)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     cᵒᶜ = esm.interfaces.ocean_properties.heat_capacity
-    frazil_temperature_flux = 1 / (ρᵒᶜ * cᵒᶜ) * frazil_heat_flux(esm)
-    return Field(frazil_temperature_flux)
+    return 1 / (ρᵒᶜ * cᵒᶜ) * frazil_heat_flux(esm)
 end
 
 """
@@ -22,8 +21,7 @@ Return the net temperature flux (K m s⁻¹) at the ocean's surface in a coupled
 """
 function net_ocean_temperature_flux(esm::EarthSystemModel)
     Jᵀ = esm.ocean.model.tracers.T.boundary_conditions.top.condition
-    net_ocean_temperature_flux = Jᵀ + frazil_temperature_flux(esm)
-    return Field(net_ocean_temperature_flux)
+    return Jᵀ + frazil_temperature_flux(esm)
 end
 
 
@@ -36,8 +34,7 @@ in a coupled `esm`.
 function sea_ice_ocean_temperature_flux(esm::EarthSystemModel)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     cᵒᶜ = esm.interfaces.ocean_properties.heat_capacity
-    sea_ice_ocean_temperature_flux = 1 / (ρᵒᶜ * cᵒᶜ) * sea_ice_ocean_heat_flux(esm)
-    return Field(sea_ice_ocean_temperature_flux)
+    return 1 / (ρᵒᶜ * cᵒᶜ) * sea_ice_ocean_heat_flux(esm)
 end
 
 
@@ -48,9 +45,7 @@ Return the atmosphere-ocean temperature flux (K m s⁻¹) at the atmosphere-ocea
 interface in a coupled `esm`.
 """
 function atmosphere_ocean_temperature_flux(esm::EarthSystemModel)
-    atmosphere_ocean_temperature_flux =
-        net_ocean_temperature_flux(esm) - sea_ice_ocean_temperature_flux(esm)
-    return Field(atmosphere_ocean_temperature_flux)
+    return net_ocean_temperature_flux(esm) - sea_ice_ocean_temperature_flux(esm)
 end
 
 
@@ -64,8 +59,7 @@ end
 Return the two-dimensional frazil heat flux (W m⁻²) in a coupled `esm`.
 """
 function frazil_heat_flux(esm::EarthSystemModel)
-    frazil_heat_flux = esm.interfaces.sea_ice_ocean_interface.fluxes.frazil_heat
-    return frazil_heat_flux
+    return esm.interfaces.sea_ice_ocean_interface.fluxes.frazil_heat
 end
 
 """
@@ -76,8 +70,7 @@ Return the net heat flux (W m⁻²) at the ocean's surface in a coupled `esm`.
 function net_ocean_heat_flux(esm::EarthSystemModel)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     cᵒᶜ = esm.interfaces.ocean_properties.heat_capacity
-    net_ocean_heat_flux = ρᵒᶜ * cᵒᶜ * net_ocean_temperature_flux(esm)
-    return Field(net_ocean_heat_flux)
+    return ρᵒᶜ * cᵒᶜ * net_ocean_temperature_flux(esm)
 end
 
 """
@@ -87,9 +80,7 @@ Return the sea ice-ocean heat flux (W m⁻²) at the sea ice-ocean interface
 in a coupled `esm`.
 """
 function sea_ice_ocean_heat_flux(esm::EarthSystemModel)
-    sea_ice_ocean_heat_flux =
-        esm.interfaces.sea_ice_ocean_interface.fluxes.interface_heat + frazil_heat_flux(esm)
-    return Field(sea_ice_ocean_heat_flux)
+    return esm.interfaces.sea_ice_ocean_interface.fluxes.interface_heat + frazil_heat_flux(esm)
 end
 
 """
@@ -101,8 +92,7 @@ interface in a coupled `esm`.
 function atmosphere_ocean_heat_flux(esm::EarthSystemModel)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     cᵒᶜ = esm.interfaces.ocean_properties.heat_capacity
-    atmosphere_ocean_heat_flux = ρᵒᶜ * cᵒᶜ * atmosphere_ocean_temperature_flux(esm)
-    return Field(atmosphere_ocean_heat_flux)
+    return ρᵒᶜ * cᵒᶜ * atmosphere_ocean_temperature_flux(esm)
 end
 
 
@@ -116,8 +106,7 @@ end
 Return the net salinity flux (g/kg m s⁻¹) at the ocean's surface in a coupled `esm`.
 """
 function net_ocean_salinity_flux(esm::EarthSystemModel)
-    Jˢ = esm.ocean.model.tracers.S.boundary_conditions.top.condition
-    return Jˢ
+    return esm.ocean.model.tracers.S.boundary_conditions.top.condition
 end
 
 
@@ -128,8 +117,7 @@ Return the sea ice-ocean salinity flux (g/kg m s⁻¹) at the sea ice-ocean inte
 in a coupled `esm`.
 """
 function sea_ice_ocean_salinity_flux(esm::EarthSystemModel)
-    sea_ice_ocean_salinity_flux = esm.interfaces.sea_ice_ocean_interface.fluxes.salt
-    return sea_ice_ocean_salinity_flux
+    return esm.interfaces.sea_ice_ocean_interface.fluxes.salt
 end
 
 """
@@ -139,9 +127,7 @@ Return the atmosphere-ocean salinity flux (g/kg m s⁻¹) at the atmosphere-ocea
 interface in a coupled `esm`.
 """
 function atmosphere_ocean_salinity_flux(esm::EarthSystemModel)
-    atmosphere_ocean_salinity_flux =
-        net_ocean_salinity_flux(esm) - sea_ice_ocean_salinity_flux(esm)
-    return Field(atmosphere_ocean_salinity_flux)
+    return net_ocean_salinity_flux(esm) - sea_ice_ocean_salinity_flux(esm)
 end
 
 
@@ -157,8 +143,7 @@ Return the net freshwater mass flux (kg m⁻² s⁻¹) at the ocean's surface in
 function net_ocean_freshwater_flux(esm::EarthSystemModel; reference_salinity = 35)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     S₀ = convert(typeof(ρᵒᶜ), reference_salinity)
-    net_ocean_frashwater_flux = - ρᵒᶜ / S₀ * net_ocean_salinity_flux(esm)
-    return Field(net_ocean_frashwater_flux)
+    return - ρᵒᶜ / S₀ * net_ocean_salinity_flux(esm)
 end
 
 """
@@ -170,8 +155,7 @@ in a coupled `esm`.
 function sea_ice_ocean_freshwater_flux(esm::EarthSystemModel; reference_salinity = 35)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     S₀ = convert(typeof(ρᵒᶜ), reference_salinity)
-    sea_ice_ocean_freshwater_flux = - ρᵒᶜ / S₀ * sea_ice_ocean_salinity_flux(esm)
-    return Field(sea_ice_ocean_freshwater_flux)
+    return - ρᵒᶜ / S₀ * sea_ice_ocean_salinity_flux(esm)
 end
 
 """
@@ -183,6 +167,5 @@ interface in a coupled `esm`.
 function atmosphere_ocean_freshwater_flux(esm::EarthSystemModel; reference_salinity = 35)
     ρᵒᶜ = esm.interfaces.ocean_properties.reference_density
     S₀ = convert(typeof(ρᵒᶜ), reference_salinity)
-    atmosphere_ocean_freshwater_flux = - ρᵒᶜ / S₀ * atmosphere_ocean_salinity_flux(esm)
-    return Field(atmosphere_ocean_freshwater_flux)
+    return - ρᵒᶜ / S₀ * atmosphere_ocean_salinity_flux(esm)
 end
