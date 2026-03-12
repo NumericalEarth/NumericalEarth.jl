261276462c99337db0f9f5415a79b8f1ab306796
Simone Silvestri <silvestri.simone0@gmail.com>
2026-03-12 15:57:15 +0100

    spit out warning

--- src/DataWrangling/metadata_field.jl
+++ src/DataWrangling/metadata_field.jl
@@ -168,6 +168,16 @@ function set!(target_field::Field, metadata::Metadatum; kw...)
     grid = target_field.grid
     arch = child_architecture(grid)
     meta_field = Field(metadata, arch; kw...)
+
+    Lzt = grid.Lz
+    Lzm = meta_field.grid.Lz
+    
+    if Lzt > Lzm
+        @warn "The vertical range of the $(metadata.dataset) dataset ($(Lzm) m) is smaller than " *
+              "the target grid ($(Lzt) m). Some vertical levels will not be filled with data, " *
+              "possibly leading to numerical errors."
+    end
+
     interpolate!(target_field, meta_field)
     return target_field
 end
