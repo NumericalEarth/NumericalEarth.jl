2fc712dbe619f0593ad277a04b922db9b979dc84
simone-silvestri <silvestri.simone0@gmail.com>
2026-03-24 06:49:10 -0400

    add some stuff

--- diagnostic_runs_1.jl
+++
@@ -1,7 +0,0 @@
-include("diagnostic_runs.jl")
-
-static_grid = default_one_degree_grid(; mutable=false, active_cells_map=false)
-static_ocean_kwargs = default_ocean_configuration(static_grid)
-static_ocean_kwargs = merge(static_ocean_kwargs, (momentum_advection=nothing, tracer_advection=nothing))
-# Run #1 (default)
-run_one_degree_diagnostic("static_noadvection_noactivecells", GPU(); grid = static_grid, ocean_kwargs = static_ocean_kwargs)
