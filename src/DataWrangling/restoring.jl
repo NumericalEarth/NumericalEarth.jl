d99b2b618522d994d01b85f343e97f37fc04142c
Simone Silvestri <silvestri.simone0@gmail.com>
2026-04-15 12:48:44 +0200

    Misc cleanups and refactors
    - Bathymetry: minor fix in regrid_bathymetry
    - DataWrangling: inpainting and restoring improvements
    - Diagnostics: MLD minor update
    - EarthSystemModels: simplify time stepping, minor earth_system_model cleanup
    - InterfaceComputations: minor refactors in sea_ice_ocean fluxes and heat flux formulations
    
    Note: Ocean simulation refactoring (flux_and_restoring, Oceans.jl, ocean_simulation.jl)
    and Diagnostics (interface_fluxes) are handled in PR #155 and #158.

--- src/DataWrangling/restoring.jl
+++ src/DataWrangling/restoring.jl
@@ -12,6 +12,7 @@ using Dates: Second
 
 import NumericalEarth: stateindex
 import Oceananigans.Forcings: materialize_forcing
+import Oceananigans.OutputReaders: extract_field_time_series
 
 # Variable names for restorable data
 struct Temperature end
@@ -234,6 +235,7 @@ function Base.show(io::IO, dsr::DatasetRestoring)
 end
 
 materialize_forcing(forcing::DatasetRestoring, field, field_name, model_field_names) = forcing
+extract_field_time_series(forcing::DatasetRestoring) = forcing.field_time_series
 
 #####
 ##### Masks for restoring
