86439c01d1f2e5a851bc9de6c38c0d9856c29663
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2026-04-16 10:15:30 -0600

    more of that

--- test/test_reactant.jl
+++ test/test_reactant.jl
@@ -1,6 +1,5 @@
 using Test
 using Reactant
-using NumericalEarth.EarthSystemModels: initialization_update_state!
 using Oceananigans: Oceananigans
 using Oceananigans.Architectures: ReactantState
 using Oceananigans.Grids: Bounded, Flat, LatitudeLongitudeGrid, Periodic
