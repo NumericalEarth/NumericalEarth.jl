e027bd00d83690e71670badbe901cfcdf8173bb2
Simone Silvestri <silvestri.simone0@gmail.com>
2026-04-22 15:57:34 +0200

    Update ext/NumericalEarthArchGDALExt.jl

--- ext/NumericalEarthArchGDALExt.jl
+++ ext/NumericalEarthArchGDALExt.jl
@@ -6,7 +6,7 @@ using NCDatasets
 
 import NumericalEarth.DataWrangling.IBCAO: reproject_ibcao_to_netcdf
 
-function _reproject_ibcao_to_netcdf(tiff_path, nc_path)
+function reproject_ibcao_to_netcdf(tiff_path, nc_path)
     ArchGDAL.read(tiff_path) do src
         # Warp from EPSG:3996 (Polar Stereographic) to EPSG:4326 (WGS84)
         # at 0.01° resolution, clipping to 64–90°N
