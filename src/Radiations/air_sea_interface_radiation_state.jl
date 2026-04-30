bd251416a89f0e0c921d47e6b14227c5d203cc57
Gregory L. Wagner <wagner.greg@gmail.com>
2026-04-30 08:23:23 -0600

    Apply suggestion from @glwagner

--- src/Radiations/air_sea_interface_radiation_state.jl
+++ src/Radiations/air_sea_interface_radiation_state.jl
@@ -15,8 +15,7 @@
     return (; σ, α, ϵ, ℐꜜˢʷ, ℐꜜˡʷ)
 end
 
-@inline function air_sea_ice_interface_radiation_state(rk, exchanger_state,
-                                                       i, j, k, grid, time)
+@inline function air_sea_ice_interface_radiation_state(rk, exchanger_state, i, j, k, grid, time)
     σ = rk.σ
     @inbounds ℐꜜˢʷ = exchanger_state.ℐꜜˢʷ[i, j, 1]
     @inbounds ℐꜜˡʷ = exchanger_state.ℐꜜˡʷ[i, j, 1]
