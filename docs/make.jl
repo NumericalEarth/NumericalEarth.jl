15f3588aa34737295f42188291264cb9046be6f9
Eliot Quon <eliot@aeolus.earth>
2026-05-01 10:43:57 -0600

    Now, literate

--- docs/make.jl
+++ docs/make.jl
@@ -35,7 +35,8 @@ examples = [
     Example("Global climate simulation", "global_climate_simulation", false),
     Example("Veros ocean simulation", "veros_ocean_forced_simulation", false),
     Example("Breeze over two oceans", "breeze_over_two_oceans", false),
-    Example("ERA5 winds and Stokes drift", "ERA5_winds_and_stokes_drift", true),
+    Example("ERA5 surface winds and Stokes drift", "ERA5_winds_and_stokes_drift", true),
+    Example("ERA5 atmosphere", "ERA5_atmosphere", false),
 ]
 
 # Developer examples from docs/src/developers/ directory
