e65b30a62b152a2e1f182a1409fe1e50238de69b
Simone Silvestri <silvestri.simone0@gmail.com>
2026-05-04 16:10:59 +0200

    try like this

--- ext/NumericalEarthSpeedyWeatherExt/speedy_weather_exchanger.jl
+++ ext/NumericalEarthSpeedyWeatherExt/speedy_weather_exchanger.jl
@@ -4,7 +4,7 @@ using Oceananigans.Grids: architecture
 using Oceananigans.Utils: launch!
 using Oceananigans.Operators: intrinsic_vector
 
-using ConservativeRegridding: Regridder
+using ConservativeRegridding: Regridder, best_manifold
 import ConservativeRegridding: regrid!
 
 using NumericalEarth.EarthSystemModels: sea_ice_concentration
@@ -27,7 +27,7 @@ function ComponentExchanger(atmosphere::SpeedySimulation, exchange_grid)
     spectral_grid = atmosphere.model.spectral_grid
     # Use the exchange_grid's manifold for both grids to avoid
     # radius mismatch between Oceananigans and SpeedyWeather.
-    manifold = GOCore.best_manifold(exchange_grid)
+    manifold = best_manifold(exchange_grid)
     from_atmosphere = Regridder(manifold, exchange_grid, spectral_grid)
     to_atmosphere   = Regridder(manifold, spectral_grid, exchange_grid)
     regridder = (; to_atmosphere, from_atmosphere)
