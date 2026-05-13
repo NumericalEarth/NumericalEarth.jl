04efc1956aace6a49fbd2b44f23e6cff73f37b85
Taimoor Sohail <taimoorsohail@gmail.com>
2026-05-13 11:43:09 +1000

    Add ZeroField to Diagnostics.jl imports

--- src/Diagnostics/Diagnostics.jl
+++ src/Diagnostics/Diagnostics.jl
@@ -12,7 +12,7 @@ using Oceananigans.Architectures: architecture
 using Oceananigans.Models: buoyancy_operation
 using Oceananigans.Grids: new_data, inactive_cell, znode
 using Oceananigans.BoundaryConditions: FieldBoundaryConditions, fill_halo_regions!
-using Oceananigans.Fields: FieldStatus
+using Oceananigans.Fields: FieldStatus, ZeroField
 using Oceananigans.Utils: launch!
 using KernelAbstractions: @index, @kernel
 using Oceananigans.BoundaryConditions: DiscreteBoundaryFunction
