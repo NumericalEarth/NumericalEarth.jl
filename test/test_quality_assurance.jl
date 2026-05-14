e8fa37c959f4db08c66dc606a40bd2ab6742b3e7
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-14 11:44:28 +1000

    add ExplicitImports qa

---
+++ test/test_quality_assurance.jl
@@ -0,0 +1,36 @@
+using Test
+using NumericalEarth
+using ExplicitImports: ExplicitImports
+
+@testset "ExplicitImports" begin
+    modules = (
+        NumericalEarth,
+        NumericalEarth.Atmospheres,
+        NumericalEarth.Bathymetry,
+        NumericalEarth.DataWrangling,
+        NumericalEarth.DataWrangling.ECCO,
+        NumericalEarth.DataWrangling.EN4,
+        NumericalEarth.DataWrangling.ERA5,
+        NumericalEarth.DataWrangling.ETOPO,
+        NumericalEarth.DataWrangling.GEBCO,
+        NumericalEarth.DataWrangling.GLORYS,
+        NumericalEarth.DataWrangling.IBCAO,
+        NumericalEarth.DataWrangling.IBCSO,
+        NumericalEarth.DataWrangling.JRA55,
+        NumericalEarth.DataWrangling.ORCA,
+        NumericalEarth.DataWrangling.OSPapa,
+        NumericalEarth.DataWrangling.WOA,
+        NumericalEarth.Diagnostics,
+        NumericalEarth.EarthSystemModels,
+        NumericalEarth.EarthSystemModels.InterfaceComputations,
+        NumericalEarth.InitialConditions,
+        NumericalEarth.Lands,
+        NumericalEarth.Oceans,
+        NumericalEarth.Radiations,
+        NumericalEarth.SeaIces,
+    )
+
+    @testset "Explicit Imports [$(mod)]" for mod in modules
+        @test ExplicitImports.check_no_implicit_imports(mod) === nothing
+    end
+end
