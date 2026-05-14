918d4895abdb78551024001d7c38d0517eeb93c7
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-14 18:10:09 +1000

    cleanup

--- ext/NumericalEarthBreezeExt/NumericalEarthBreezeExt.jl
+++ ext/NumericalEarthBreezeExt/NumericalEarthBreezeExt.jl
@@ -10,12 +10,7 @@ using Oceananigans.Architectures: architecture
 using Oceananigans.BoundaryConditions: FluxBoundaryCondition, FieldBoundaryConditions
 using Oceananigans.Utils: launch!
 
-import NumericalEarth.EarthSystemModels: thermodynamics_parameters,
-                                          surface_layer_height,
-                                          boundary_layer_height,
-                                          interpolate_state!,
-                                          update_net_fluxes!
-import NumericalEarth.EarthSystemModels.InterfaceComputations: ComponentExchanger, net_fluxes, computed_fluxes
+using NumericalEarth.EarthSystemModels.InterfaceComputations: computed_fluxes
 
 include("breeze_atmosphere_interface.jl")
 include("breeze_atmosphere_simulation.jl")
