83e96101e535d0f5e7c1c1fca6b524834d194577
Simone Silvestri <silvestri.simone0@gmail.com>
2026-05-15 11:25:33 +0200

    fix download progress

--- src/DataWrangling/DataWrangling.jl
+++ src/DataWrangling/DataWrangling.jl
@@ -36,36 +36,38 @@ import Oceananigans.Fields: set!
 ##### Downloading utilities
 #####
 
-next_fraction = Ref(0.0)
-download_start_time = Ref(time_ns())
+Base.@kwdef struct DownloadProgress
+    next_fraction :: Base.RefValue{Float64} = Ref(0.0)
+    download_start_time :: Base.RefValue{UInt64} = Ref(time_ns())
+end
 
 """
-    download_progress(total, now; filename="")
+    DownloadProgress(total, now; filename="")
 """
-function download_progress(total, now; filename="")
+function (d::DownloadProgress)(total, now; filename="")
     messages = 10
 
     if total > 0
         fraction = now / total
 
-        if fraction < 1 / messages && next_fraction[] == 0
+        if fraction < 1 / messages && d.next_fraction[] == 0
             @info @sprintf("Downloading %s (size: %s)...", filename, pretty_filesize(total))
-            next_fraction[] = 1 / messages
-            download_start_time[] = time_ns()
+            d.next_fraction[] = 1 / messages
+            d.download_start_time[] = time_ns()
         end
 
-        if fraction > next_fraction[]
-            elapsed = 1e-9 * (time_ns() - download_start_time[])
+        if fraction > d.next_fraction[]
+            elapsed = 1e-9 * (time_ns() - d.download_start_time[])
             msg = @sprintf(" ... downloaded %s (%d%% complete, %s)", pretty_filesize(now),
                            100fraction, prettytime(elapsed))
             @info msg
-            next_fraction[] = next_fraction[] + 1 / messages
+            d.next_fraction[] = d.next_fraction[] + 1 / messages
         end
     else
-        if now > 0 && next_fraction[] == 0
+        if now > 0 && d.next_fraction[] == 0
             @info "Downloading $filename..."
-            next_fraction[] = 1 / messages
-            download_start_time[] = time_ns()
+            d.next_fraction[] = 1 / messages
+            d.download_start_time[] = time_ns()
         end
     end
 
