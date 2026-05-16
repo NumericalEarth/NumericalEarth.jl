d3ced54f04ca432f3176b8b5e8c66ddd6c8e7963
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-16 12:04:57 +1000

    Add AbstractPrescribedComponent abstract type (#228)
    * add PrescribedModelComponent type
    
    * add test
    
    * use PrescribedModelComponent in test
    
    * cleanup
    
    * chmode 644
    
    * align
    
    * less verbose
    
    * fix test
    
    * fix chmod
    
    * test model+components when checkpointing
    
    * no need for is_prescribed_atmosphere method
    
    * fix docstring
    
    * reorder exports for better groupping
    
    * reorder datasets
    
    * PrescribedModelComponent -> AbstractPrescribedComponent
    
    * remove stray ]

--- src/Lands/Lands.jl
+++ src/Lands/Lands.jl
@@ -3,20 +3,20 @@ module Lands
 export PrescribedLand
 
 using Oceananigans
-using Oceananigans.Utils: launch!
 using Oceananigans.Fields: Center, Face
 using Oceananigans.Grids: grid_name, architecture, prettysummary
 using Oceananigans.OutputReaders: FieldTimeSeries, update_field_time_series!, extract_field_time_series
 using Oceananigans.TimeSteppers: Clock, tick!
 using Oceananigans.Units: Time
+using Oceananigans.Utils: launch!
 
 using KernelAbstractions: @kernel, @index
+using NumericalEarth.EarthSystemModels: AbstractPrescribedComponent
 using NumericalEarth.EarthSystemModels.InterfaceComputations: interface_kernel_parameters
 
 import Oceananigans.TimeSteppers: time_step!, update_state!
 
-import NumericalEarth.EarthSystemModels: interpolate_state!,
-                                         update_net_fluxes!
+import NumericalEarth.EarthSystemModels: interpolate_state!, update_net_fluxes!
 
 import NumericalEarth.EarthSystemModels.InterfaceComputations: ComponentExchanger, initialize!
 
