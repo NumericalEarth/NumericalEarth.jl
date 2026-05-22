0e1474b48992f59ae8b991c149cc61c7d5c50469
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-22 13:30:38 +1000

    import reference density

--- src/Oceans/Oceans.jl
+++ src/Oceans/Oceans.jl
@@ -32,7 +32,8 @@ using ..EarthSystemModels: EarthSystemModels,
                            ocean_surface_velocities,
                            ocean_surface_salinity,
                            DegreesKelvin,
-                           heat_capacity
+                           heat_capacity,
+                           reference_density
 using ..EarthSystemModels.InterfaceComputations: ComponentExchanger
 
 default_gravitational_acceleration = Oceananigans.defaults.gravitational_acceleration
