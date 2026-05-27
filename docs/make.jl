fb8bf0f15b78761a46880e65f804f6f0b21d2acc
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-05-27 14:09:55 +1000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -37,13 +37,13 @@ mkpath(OUTPUT_DIR)
 # Set `build_always = false` for long-running examples that should only be built
 # on pushes to `main`/tags, or when the `build all examples` label is added to a PR.
 examples = [
-    # Example("Single-column ocean simulation", "single_column_os_papa_simulation", true),
-    # Example("One-degree ocean--sea ice simulation", "one_degree_simulation", false),
-    # Example("Near-global ocean simulation", "near_global_ocean_simulation", false),
-    # Example("Global climate simulation", "global_climate_simulation", false),
-    Example("Veros ocean simulation", "veros_ocean_forced_simulation", true),
-    # Example("Breeze over four oceans", "breeze_over_four_oceans", false),
-    #Example("ERA5 hourly data", "ERA5_hourly_data", true),
+    Example("Single-column ocean simulation", "single_column_os_papa_simulation", true),
+    Example("One-degree ocean--sea ice simulation", "one_degree_simulation", false),
+    Example("Near-global ocean simulation", "near_global_ocean_simulation", false),
+    Example("Global climate simulation", "global_climate_simulation", false),
+    Example("Veros ocean simulation", "veros_ocean_forced_simulation", false),
+    Example("Breeze over four oceans", "breeze_over_four_oceans", false),
+    Example("ERA5 hourly data", "ERA5_hourly_data", true),
 ]
 
 # Developer examples from docs/src/developers/ directory
