adcfbd6ed07c86978dd9d84cf05b7096e937fa71
kaiyuan-cheng <74800123+kaiyuan-cheng@users.noreply.github.com>
2026-05-29 10:51:38 -0400

    Update src/Lands/hydrology/dry_land.jl
    Co-authored-by: Gregory L. Wagner <wagner.greg@gmail.com>

--- src/Lands/hydrology/dry_land.jl
+++ src/Lands/hydrology/dry_land.jl
@@ -9,6 +9,6 @@ struct DryLand <: AbstractHydrology end
 
 flux_variables(::DryLand) = ()
 
-saturation(::DryLand, land) = 0.0
+saturation(::DryLand, land) = 0
 
 Base.summary(::DryLand) = "DryLand (moisture availability β ≡ 0)"
