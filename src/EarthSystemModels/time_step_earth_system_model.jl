b94babb6ca904d067edd7f8d29e511f24af5e633
Taimoor Sohail <tsohail@spartan-bm131.hpc.unimelb.edu.au>
2026-06-10 18:40:31 +1000

    Fix north halo filling

--- src/EarthSystemModels/time_step_earth_system_model.jl
+++ src/EarthSystemModels/time_step_earth_system_model.jl
@@ -1,4 +1,5 @@
 using ClimaSeaIce: SeaIceThermodynamics
+using Oceananigans.BoundaryConditions: fill_halo_regions!
 using Oceananigans.TimeSteppers: maybe_prepare_first_time_step!
 
 using .InterfaceComputations: compute_atmosphere_ocean_fluxes!,
@@ -10,6 +11,24 @@ using .InterfaceComputations: compute_atmosphere_ocean_fluxes!,
 # `coupled_model.radiation === nothing`).
 apply_air_sea_radiative_fluxes!(::Any) = nothing
 apply_air_sea_ice_radiative_fluxes!(::Any) = nothing
+
+fill_interface_flux_halos!(::Nothing) = nothing
+
+function fill_interface_flux_halos!(fluxes::InterfaceComputations.AtmosphereSurfaceFluxes)
+    fill_halo_regions!((fluxes.latent_heat, fluxes.sensible_heat, fluxes.water_vapor,
+                        fluxes.x_momentum, fluxes.y_momentum, fluxes.friction_velocity,
+                        fluxes.temperature_scale, fluxes.water_vapor_scale))
+end
+
+function fill_interface_flux_halos!(fluxes::InterfaceComputations.AtmosphereSeaIceFluxes)
+    fill_halo_regions!((fluxes.latent_heat, fluxes.sensible_heat, fluxes.water_vapor,
+                        fluxes.x_momentum, fluxes.y_momentum))
+end
+
+function fill_interface_flux_halos!(fluxes::InterfaceComputations.SeaIceOceanFluxes)
+    fill_halo_regions!((fluxes.interface_heat, fluxes.frazil_heat, fluxes.salt,
+                        fluxes.x_momentum, fluxes.y_momentum))
+end
 
 function Oceananigans.TimeSteppers.time_step!(coupled_model::EarthSystemModel, Δt; callbacks=[])
     maybe_prepare_first_time_step!(coupled_model, Δt, callbacks)
@@ -61,11 +80,18 @@ function Oceananigans.TimeSteppers.update_state!(coupled_model::EarthSystemModel
     InterfaceComputations.correct_state!(exchanger.sea_ice,    grid)
     InterfaceComputations.correct_state!(exchanger.ocean,      grid)
 
+    !isnothing(exchanger.atmosphere) && fill_halo_regions!(exchanger.atmosphere.state)
+
     # Phase 2: compute interface turbulent fluxes
     compute_atmosphere_ocean_fluxes!(coupled_model)
     compute_atmosphere_sea_ice_fluxes!(coupled_model)
     compute_atmosphere_land_fluxes!(coupled_model)
     compute_sea_ice_ocean_fluxes!(coupled_model)
+
+    fill_interface_flux_halos!(isnothing(coupled_model.interfaces.atmosphere_ocean_interface) ? nothing : coupled_model.interfaces.atmosphere_ocean_interface.fluxes)
+    fill_interface_flux_halos!(isnothing(coupled_model.interfaces.atmosphere_sea_ice_interface) ? nothing : coupled_model.interfaces.atmosphere_sea_ice_interface.fluxes)
+    fill_interface_flux_halos!(isnothing(coupled_model.interfaces.sea_ice_ocean_interface) ? nothing : coupled_model.interfaces.sea_ice_ocean_interface.fluxes)
+    fill_interface_flux_halos!(isnothing(coupled_model.interfaces.atmosphere_land_interface) ? nothing : coupled_model.interfaces.atmosphere_land_interface.fluxes)
 
     # Phase 3: assemble net component fluxes (turbulent only)
     update_net_fluxes!(coupled_model, radiation)
@@ -73,6 +99,10 @@ function Oceananigans.TimeSteppers.update_state!(coupled_model::EarthSystemModel
     update_net_fluxes!(coupled_model, land)
     update_net_fluxes!(coupled_model, sea_ice)
     update_net_fluxes!(coupled_model, ocean)
+
+    net_ocean_fluxes = coupled_model.interfaces.net_fluxes.ocean
+    fill_halo_regions!((net_ocean_fluxes.u, net_ocean_fluxes.v, net_ocean_fluxes.T, net_ocean_fluxes.S))
+    fill_halo_regions!(coupled_model.interfaces.net_fluxes.sea_ice)
 
     # Phase 4: add radiative contributions on top
     apply_air_sea_radiative_fluxes!(coupled_model)
