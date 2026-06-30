1e459aff092f0ef5125a5ddad39656065e6d2528
Xin Kai Lee <xinkai@mit.edu>
2026-06-30 15:01:44 -0700

    Update ext/NumericalEarthReactantExt.jl
    Co-authored-by: Simone Silvestri <silvestri.simone0@gmail.com>

--- ext/NumericalEarthReactantExt.jl
+++ ext/NumericalEarthReactantExt.jl
@@ -19,10 +19,6 @@ const ReactantESM{R, A, L, I, O, F, C} = Union{
     EarthSystemModel{R, A, L, I, O, F, C, <:Distributed{ReactantState}},
 }
 
-# Mirror the CPU `reconcile_state!` (initialize the state-exchanger regridder,
-# then update the state), but trace-and-run each step through Reactant. Eager
-# calls would not mutate the `ReactantState` arrays, leaving the regridder's
-# fractional indices uninitialized so `interpolate_state!` reads halo cell (0, 0).
 function reconcile_state!(model::ReactantESM)
     @jit Oceananigans.initialize!(model.interfaces.exchanger, model)
     @jit Oceananigans.TimeSteppers.update_state!(model)
