adefeea47aea01122bcbfbe49cdfab55a6665c7b
Taimoor Sohail <taimoorsohail@gmail.com>
2026-07-15 22:21:42 +1000

    Fix north halo filling (#343)
    * Fix north halo filling
    
    * Apply suggestion from @simone-silvestri
    
    * Update time_step_earth_system_model.jl
    
    * Update ocean_simulation.jl
    
    * some more fixes
    
    ---------
    
    Co-authored-by: Taimoor Sohail <tsohail@spartan-bm131.hpc.unimelb.edu.au>
    Co-authored-by: Taimoor Sohail <tsohail@spartan-gpgpu016.hpc.unimelb.edu.au>
    Co-authored-by: Simone Silvestri <silvestri.simone0@gmail.com>

--- src/Oceans/Oceans.jl
+++ src/Oceans/Oceans.jl
@@ -37,7 +37,7 @@ using ..EarthSystemModels: EarthSystemModels,
                            DegreesKelvin,
                            default_stop_time,
                            heat_capacity
-using ..EarthSystemModels.InterfaceComputations: ComponentExchanger
+using ..EarthSystemModels.InterfaceComputations: InterfaceComputations, ComponentExchanger
 
 default_gravitational_acceleration = Oceananigans.defaults.gravitational_acceleration
 default_planet_rotation_rate = Oceananigans.defaults.planet_rotation_rate
